ae7ed8597031531929ff197286913ae4e3b9642d
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2024-02-18 19:23:19 -0300

    feat: created todos crud

--- app/todos/delete-todo.tsx
+++ app/todos/delete-todo.tsx
@@ -1,8 +1,11 @@
+import { deleteTodo } from "#/actions/todos";
 import { Trash2 } from "lucide-react";
 
 export function DeleteTodo({ id }: { id: string }) {
+  const deleteTodoWithId = deleteTodo.bind(null, id)
+  
   return (
-    <form className="ml-auto">
+    <form className="ml-auto" action={deleteTodoWithId}>
       <button className="flex items-center justify-center hover:text-violet-500">
         <Trash2 strokeWidth={1.5} size={20} />
       </button>
